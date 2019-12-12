65482a1cbb1bb842adf27068014503674f60129c
eslamadel11993377 <eslamadel11993377@gmail.com>
2019-12-12 18:56:19 +0200

    Mips

--- src/com/assembler/Main.java
+++ src/com/assembler/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-//        Parser parser=new Parser("aaa     ::");
-        String x="$s0        a,,,$s1";
+        Parser parser=new Parser("aaa     ::");
+        /*String x="$s0        a,,,$s1";
         String[]arr=x.split(",");
         System.out.println(arr.length);
         for(int i=0;i<arr.length;i++)
-            System.out.println(arr[i]+"      "+arr[i].length());
+            System.out.println(arr[i]+"      "+arr[i].length());*/
     }
 }
