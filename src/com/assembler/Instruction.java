b3136e70dcd581499e17a0636b2ec6595262aced
“AhmedOsama” <gqozgak1@gmail.com>
2019-12-12 16:29:21 +0200

    intial1

---
+++ src/com/assembler/Instruction.java
@@ -0,0 +1,25 @@
+package com.assembler;
+
+import java.util.ArrayList;
+
+public class Instruction {
+    String instruct;
+    String labelName;
+    ArrayList<String>registers=new ArrayList<>();
+    Boolean isLabel=false;
+    Boolean rType=false;
+    Boolean iType=false;
+    Boolean jType=false;
+
+    public Instruction() {
+    }
+
+    public Instruction(String instruct, ArrayList<String> registers, Boolean isLabel, Boolean rType, Boolean iType, Boolean jType) {
+        this.instruct = instruct;
+        this.registers = registers;
+        this.isLabel = isLabel;
+        this.rType = rType;
+        this.iType = iType;
+        this.jType = jType;
+    }
+}
