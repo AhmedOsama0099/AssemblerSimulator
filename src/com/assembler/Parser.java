e98d17792fbac7b21bdc227be3aa96e4f9c2bf8a
“AhmedOsama” <gqozgak1@gmail.com>
2019-12-12 18:29:12 +0200

    instruction class

--- src/com/assembler/Parser.java
+++ src/com/assembler/Parser.java
@@ -10,9 +10,9 @@ public class Parser {
     private static ArrayList<Instruction> codeLines = new ArrayList<>();
     public final ArrayList<String> instructions = new ArrayList<>(
 
-            Arrays.asList("add","sub","and","or","sll","slt","lw", "sw",
-                     "addi", "andi", "ori", "slti", "lui",
-                     "jr", "j", "beq", "bne"
+            Arrays.asList("add", "sub", "and", "or", "sll", "slt", "lw", "sw",
+                    "addi", "andi", "ori", "slti", "lui",
+                    "jr", "j", "beq", "bne"
             ));
 
     Parser(String inputCode) {
@@ -21,68 +21,80 @@ public class Parser {
     }
 
     private String splitCodeAndLabelsAndCheckValidate(String inputCode) {
-        String exceptions="";
+        String exceptions = "";
         //String instruction="";
         boolean op = true;//first one is command
-        Instruction instruction=new Instruction();
+        Instruction instruction;
         String line;
         ///String label="";
         int checkLabel;
-        int currentLine=1;
+        int currentLine = 0;
         BufferedReader reader = new BufferedReader(new StringReader(inputCode));
         try {
             while ((line = reader.readLine()) != null) {
-                if(line=="\n")
+                currentLine++;
+                instruction = new Instruction();
+                if (line == "\n")
                     continue;
-                line=line.trim();
-                checkLabel=line.indexOf(":");
-                if(checkLabel!=-1){
-                    if(checkLabel!=line.length()-1){
-                        exceptions+="error label definition at line "+currentLine+"\n";
-                    }
-                    else if(line.length()==1){
-                        exceptions+="no label name at line "+currentLine+"\n";
-                    }else{
-                        instruction.isLabel=true;
-                        String labelName=line.substring(0,checkLabel-1);
-                        instruction.labelName=(labelName.trim());
+                line = line.trim();
+                checkLabel = line.indexOf(":");
+                if (checkLabel != -1) {
+                    if (checkLabel != line.length() - 1) {
+                        exceptions += "error label definition at line " + currentLine + "\n";
+                    } else if (line.length() == 1) {
+                        exceptions += "no label name at line " + currentLine + "\n";
+                    } else {
+                        instruction.isLabel = true;
+                        String labelName = line.substring(0, checkLabel - 1);
+                        instruction.labelName = (labelName.trim());
                         System.out.println(labelName.trim());
                         codeLines.add(instruction);
-                        instruction=new Instruction();
+                        //instruction=new Instruction();
                     }
-                }
-                else{
-                    line=line.trim();
-                    instruction.instruct="";
-                    boolean isInstruct=true;
-                    String tmp="";
-                    for(int i=0;i<line.length();i++){
-                        tmp+=line.charAt(i);
-                        if(!isInstruct)
-                            tmp=tmp.trim();
-                        if(isInstruct&&i==line.length()-1){
-                            if(instructions.indexOf(tmp)==-1)
-                                exceptions+="incorrect instruct at line "+currentLine+"\n";
+                } else {
+                    line = line.trim();
+                    instruction.instruct = "";
+                    // boolean isInstruct = true;
+                    String tmp = "";
+                    for (int i = 0; i < line.length(); i++) {
+                        tmp += line.charAt(i);
+                        tmp = tmp.trim();
+                        if (i == line.length() - 1) {
+                            if (instructions.indexOf(tmp) == -1)
+                                exceptions += "incorrect instruct at line " + currentLine + "\n";
                             else
-                                exceptions+="too few argument at line "+currentLine+"\n";
+                                exceptions += "too few argument at line " + currentLine + "\n";
                             continue;
                         }
-                        if(tmp.charAt(tmp.length()-1)==' '&&isInstruct){
-                            isInstruct=false;
-                            tmp=tmp.trim();
-                            if(instructions.indexOf(tmp)==-1){
-                                exceptions+="incorrect instruct at line "+currentLine+"\n";
+                        if (line.charAt(i) == ' ' || line.charAt(i) == '$') {
+                            //isInstruct = false;
+                            tmp = tmp.trim();
+                            if (tmp.charAt(tmp.length() - 1) == '$') {
+                                StringBuilder sb = new StringBuilder(tmp);
+                                sb.deleteCharAt(tmp.length() - 1);
+                                tmp = sb.toString();
+                            }
+                            int instructionIndex = instructions.indexOf(tmp);
+                            if (instructionIndex == -1) {
+                                exceptions += "incorrect instruct at line " + currentLine + "\n";
+                                break;
+                            } else {
+                                instruction.instruct = tmp;
                                 break;
                             }
-                            else{
-                                instruction.instruct=tmp;
-                            }
-
                         }
-
+                    }
+                    line=line.substring(tmp.length()+1);
+                    line=line.trim();
+                    String []args=line.split(",");
+                    for(int i=0;i<args.length;i++){
+                        if(args[i].length()==0){
+                            exceptions+="unexpected , at line "+currentLine+"\n";
+                            break;
+                        }
+                            
                     }
                 }
-                currentLine++;
             }
 
 
